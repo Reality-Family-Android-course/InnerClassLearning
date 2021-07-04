1ac58d046dbfa1e06fcabc0cb5082e9b5b874194
artemmad <artemmakarevich1997@gmail.com>
2021-07-04 14:35:23 +0300

    Finished lesson

---
+++ src/ru/synergy/innernested/Parcel.java
@@ -0,0 +1,21 @@
+package ru.synergy.innernested;
+
+public class Parcel {
+    private String recepient;
+    private int mass;
+    /// ...
+
+    class  Destination {
+        private String street;
+        private int homeNumber;
+        private int roomNumber;
+
+        public void doSmth(){
+            System.out.println(mass);
+        }
+    }
+
+    ///...
+}
+
+
